de193b8bd42eb4101783677c91337e3b9ec0c7f8
@RyanRY27 <magdalitaryan7@gmail.com>
2025-09-20 22:38:06 +0800

    move nav.jsx to components folder

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 import Whatweoffer from "./views/Homepage";
